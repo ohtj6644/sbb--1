d44d83e772fc04f52160319af51889570c03028e
깃ohtj6644 <ohtj6644@naver.com>
2023-05-02 13:44:22 +0900

    0502

---
+++ src/main/java/com/example/sbb/Answer.java
@@ -0,0 +1,20 @@
+package com.example.sbb;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+@CreatedDate
+    private LocalDateTime createDate;
+@ManyToOne
+    private Question question;
+}
--- src/main/java/com/example/sbb/MainController.java
+++ src/main/java/com/example/sbb/MainController.java
@@ -2,14 +2,43 @@ package com.example.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 //손님이 왔을때 처리해주는 종업원 같은 역할 컨트롤러 .
+@Controller
 public class MainController {
-    @GetMapping("/sbb")
+    @GetMapping("/minus")
     @ResponseBody
-    public String index(){
-        return "안녕하세요 sbb에 오신것을 환영합니다";
+    public int showMinus(int a, int b) {
+        return a - b;
     }
+
+
+    int c;
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public int showIncrease() {
+
+        return c++;
+
+    }
+
+    @GetMapping("/Mbti/{name}")
+    @ResponseBody
+    public String showMbti(@PathVariable String name) {
+        return switch (name) {
+            case "오택주" -> {
+                char j = 'J';
+                yield "IST"+j;
+            }
+            case "홍길순", "임꺽정" -> "ENFP";
+            case "반장" -> "ESTJ";
+            default -> "모름";
+
+        };
+    }
+
+
 }
