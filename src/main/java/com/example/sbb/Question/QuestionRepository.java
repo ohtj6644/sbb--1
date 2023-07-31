3530cf61727567d0bac46130ff8b9e4609e0e728
ohtj6644 <ohtj6644@github.com>
2023-07-31 17:02:12 +0900

    깃헙액션 배포

--- src/main/java/com/example/sbb/Question/QuestionRepository.java
+++ src/main/java/com/example/sbb/Question/QuestionRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
